8d1feeb1bd8d3d60afbb11999f493acb0e0b824f
Lee Raulin <leeraulin@gmail.com>
2020-10-19 14:16:27 -0400

    ready to test!

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useRef, createRef, useEffect } from "react";
+import { useState } from "react";
 import "./App.css";
 import DrumPad from "./DrumPad";
 import { bankOne as sounds } from "./sounds";
 
 const App = () => {
+  const [lastPressed, setLastPressed] = useState("");
   const rootRef = useRef();
   const keyRefs = sounds.reduce((a, c) => {
     a[c.keyTrigger] = createRef();
@@ -12,10 +14,11 @@ const App = () => {
 
   useEffect(() => {
     const handleKeyDown = (event) => {
-      const ref = keyRefs[event.key.toUpperCase()];
-      console.log(event.key);
+      const key = event.key.toUpperCase();
+      const ref = keyRefs[key];
       if (!ref || !ref.current || ref.current.contains(event.target)) return;
       ref.current.play();
+      setLastPressed(sounds.filter((x) => x.keyTrigger === key)[0].id);
     };
 
     document.addEventListener("keydown", handleKeyDown);
@@ -25,22 +28,21 @@ const App = () => {
     };
 
     return cleanup;
-  }, [keyRefs]);
+  }, [keyRefs, setLastPressed]);
 
   return (
     <div id="drum-machine" ref={rootRef} tabIndex="0">
-      <div id="display">
-        {sounds.map((sound) => (
-          <DrumPad
-            id={sound.id}
-            keyTrigger={sound.keyTrigger}
-            url={sound.url}
-            keyCode={sound.keyCode}
-            key={sound.id}
-            ref={keyRefs[sound.keyTrigger]}
-          />
-        ))}
-      </div>
+      {sounds.map((sound) => (
+        <DrumPad
+          id={sound.id}
+          keyTrigger={sound.keyTrigger}
+          url={sound.url}
+          key={sound.id}
+          setLastPressed={setLastPressed}
+          ref={keyRefs[sound.keyTrigger]}
+        />
+      ))}
+      <div id="display">{lastPressed}</div>
     </div>
   );
 };
--- src/DrumPad.js
+++ src/DrumPad.js
@@ -1,27 +1,15 @@
 import React from "react";
 import { forwardRef } from "react";
 
-const DrumPad = forwardRef(({ id, keyTrigger, keyCode, url }, ref) => {
+const DrumPad = forwardRef(({ id, keyTrigger, url, setLastPressed }, ref) => {
   const handleClick = () => {
     ref.current.play();
-  };
-
-  const handleKeyPress = (event) => {
-    console.log("Key Press!");
-    event.preventDefault();
-    if (event.charCode === keyCode) {
-      ref.current.play();
-    }
+    setLastPressed(id);
   };
 
   return (
-    <div tabIndex={0} onKeyDown={(e) => handleKeyPress(e)}>
-      <button
-        className="drum-pad"
-        id={id}
-        onClick={handleClick}
-        onKeyDown={(e) => handleKeyPress(e)}
-      >
+    <div>
+      <button className="drum-pad" id={id} onClick={handleClick}>
         {keyTrigger}
         <audio className="clip" id={keyTrigger} src={url} ref={ref}></audio>
       </button>
